7d150d779590b0a33fbad1d08cc62bbd3afa74c3
leslie.alldridge <leslie.alldridge@gmail.com>
2019-11-04 21:00:37 +1300

    adding first module and assignment

---
+++ Introduction/labs/Mod1_Lab1_And_Assignment/Mod1_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student details: ");
+            Console.WriteLine("Name: ");
+            string studentName = Console.ReadLine();
+            Console.WriteLine("Brithdate (yyyy mm dd): ");
+            DateTime studentBirthdate = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine($"{studentName} was born on {studentBirthdate}");
+        }
+    }
+}
---
+++ Introduction/labs/Mod1_Lab1_And_Assignment/Mod1_Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create variables of different data types
+            // initialize with a default value
+            string firstName = "";
+            string lastName = "";
+            int age = 0;
+            string street = "";
+            string city = "";
+            string country = "";
+            DateTime birthDate;
+
+            // assign some values
+            firstName = "Leslie";
+            lastName = "Alldridge";
+            age = 18;
+            street = "102 Young Street";
+            city = "New Plymouth";
+            country = "New Zealand";
+            birthDate = new DateTime(1992, 6, 9);
+
+            // output to console window
+
+            // use simple output with just variable name
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+
+            // use placeholder style
+            Console.WriteLine(street + ", " + city + ", " + country);
+
+            // use string interpolation
+            Console.WriteLine($"Born on {birthDate} and is {age} years old");
+        }
+    }
+}
