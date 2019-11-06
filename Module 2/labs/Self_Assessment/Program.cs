dc4dad0bad841122ed1a17f04281cf56f5c78122
leslie.alldridge <leslie.alldridge@gmail.com>
2019-11-06 18:39:03 +1300

    latest assignments

---
+++ Module 2/labs/Self_Assessment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Self_Assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            string output = "";
+            for (x=0; x <8; x++)
+            {
+                output = "";
+                for (y=0; y <8; y++)
+                {
+                    if (x%2 == 0)
+                    {
+                        if (y % 2 == 0)
+                        {
+                            output += "X";
+                        }
+                        else
+                        {
+                            output += "O";
+                        }
+                    }
+                    else
+                    {
+                        if (y % 2 != 0)
+                        {
+                            output += "X";
+                        }
+                        else
+                        {
+                            output += "O";
+                        }
+                    }
+                    
+                   
+                }
+                Console.WriteLine(output);
+            }
+            
+        }
+    }
+}
